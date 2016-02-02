a3ea10db1109510e7b38b8da9fbe77235d38b91a
Ben Rowe <ben.rowe.83@gmail.com>
2016-02-03 07:53:07 +1100

    Init basic unit test

---
+++ tests/FormatterTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use Benrowe\Formatter\Formatter;
+
+class FormatterTest extends PHPUnit_Framework_TestCase
+{
+    public function testBasic()
+    {
+        $formatter = new Formatter;
+    }
+
+}
