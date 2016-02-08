d057aa3832b0af42a84324eb0ac1e0ac59ddc3ca
Ben Rowe <ben.rowe.83@gmail.com>
2016-02-09 08:06:48 +1100

    Add initial unit tests

--- tests/FormatterTest.php
+++ tests/FormatterTest.php
@@ -7,6 +7,39 @@ class FormatterTest extends PHPUnit_Framework_TestCase
     public function testBasic()
     {
         $formatter = new Formatter;
+        $this->assertSame('ben', $formatter->asRaw('ben'));
+        $this->assertSame($formatter->nullValue, $formatter->format(null, 'raw'));
+        $this->assertSame($formatter->nullValue, $formatter->format(null));
+    }
+
+    public function testCustom()
+    {
+        $formatter = new Formatter(['reverse' => function ($value) {
+
+            if ($value === null) {
+                return $this->nullValue;
+            }
+
+            return strrev($value);
+        }]);
+
+        $this->assertSame('neb', $formatter->format('ben', 'via format()'));
+        $this->assertSame('neb', $formatter->asReverse('ben'), 'via asReverse()');
+
+    }
+
+    public function testOverload()
+    {
+        $formatter = new Formatter(['raw' => function ($value) {
+            if ($value === null) {
+                return $this->nullValue;
+            }
+            return str_rot13($value);
+        }]);
+
+        $this->assertSame('ora', $formatter->asRaw('ben'));
+        $this->assertSame('ora', $formatter->format('ben'));
+        $this->assertSame($formatter->nullValue, $formatter->format(null));
     }
 
 }
