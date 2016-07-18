86d2bea444d035ca302c1231dc1c7a940d3bf1ca
Ben Rowe <ben.rowe.83@gmail.com>
2016-07-18 20:01:58 +1000

    Add FormatterProvider interface
    Classes that want to provide formatter functionality to the core Formatter must inherit from this interface

---
+++ src/FormatterProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Benrowe\Formatter;
+
+/**
+ * Formatter Provider interface
+ *
+ * Provide a standard format interface for Providers to adher to
+ *
+ * @package Benrowe\Formatter
+ */
+interface FormatterProvider
+{
+    /**
+     * Describes the formats that this formatter provides
+     *
+     * @return string[]
+     */
+    public function formats();
+
+    /**
+     * Format the corresponding value to the format provided
+     *
+     * @param  mixed $value
+     * @param  array $params
+     * @return mixed
+     */
+    public function format($value, array $params = [])
+}
