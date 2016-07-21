9dbd7954d755ccd955948fc4e55bcccda7dea22f
Ben Rowe <ben.rowe.83@gmail.com>
2016-07-21 17:52:49 +1000

    Add functionality to abstract provider for format() and hasFormat()

--- src/AbstractFormatterProvider.php
+++ src/AbstractFormatterProvider.php
@@ -4,6 +4,7 @@ namespace Benrowe\Formatter;
 
 use ReflectionClass;
 use ReflectionMethod;
+use InvalidArgumentException;
 
 /**
  * AbstractFormatterProvider
@@ -15,13 +16,14 @@ use ReflectionMethod;
  */
 abstract class AbstractFormatterProvider implements FormatterProvider
 {
+    protected $defaultFormatter;
     /**
      * method prefix
      */
     const METHOD_PATTERN_MATCH = '/^as([A-Z]\w+)$/';
 
     /**
-     * Provide a list of formatters this class makes available
+     * Provide a list of formatters this that are available from this provider
      *
      * @return array
      */
@@ -44,8 +46,56 @@ abstract class AbstractFormatterProvider implements FormatterProvider
         return $formats;
     }
 
-    public function format($value, array $params = [])
+    /**
+     * Format the supplied value, based on the desired format + configuration
+     *
+     * @param  mixed $value The value to format
+     * @param  string|array $format either the formatter name, or the formatter
+     *                              config as an array. If it's an array, the
+     *                              first item must be the same of the formatter
+     * @return mixed
+     */
+    public function format($value, $format = null)
     {
+        $format = $format ?: $this->defaultFormatter;
 
+        $params = [$value];
+
+        if (is_array($format)) {
+            if (!isset($format[0])) {
+                throw new InvalidArgumentException(
+                    'The $format must contain at least one element'
+                );
+            }
+            $tmpFormat = $format[0];
+            $format[0] = $value;
+            $params = $format;
+            $format = $tmpFormat;
+        }
+
+        if (!$this->hasFormat($format)) {
+            throw new InvalidArgumentException(
+                'Unknown format: "' . $format . '"'
+            );
+        }
+
+        // is the formatter in a custom defined
+
+        $func = [$this, 'as'.$format];
+
+        return call_user_func_array($func, $params);
+    }
+
+    /**
+     * Check if the requested format exists in this provider
+     *
+     * @param  string  $format The formatter name you want to check for
+     * @return boolean
+     */
+    public function hasFormat($format)
+    {
+        $formats = $this->formats();
+
+        return in_array($format, $formats);
     }
 }
