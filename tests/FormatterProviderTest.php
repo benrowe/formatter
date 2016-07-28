0eae2d99f053dabf1d0976ef1efbaf5e1b053499
Ben Rowe <ben.rowe.83@gmail.com>
2016-07-29 06:26:35 +1000

    Update test case for formatter providers

--- tests/FormatterProviderTest.php
+++ tests/FormatterProviderTest.php
@@ -5,8 +5,24 @@ use Benrowe\Formatter\Test\Examples\NumberSampleProvider;
 use Benrowe\Formatter\Test\Examples\SampleProvider;
 // use stdObject;
 
+/**
+ * Test sample providers
+ */
 class FormatterProviderTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * @var Formatter
+     */
+    private $formatter;
+
+    public function setUp()
+    {
+        $this->formatter = new Formatter([
+            'number' => new NumberSampleProvider,
+            'sample' => 'Benrowe\Formatter\Test\Examples\SampleProvider'
+        ]);
+    }
+
     public function testConstructor()
     {
         $formatter = new Formatter([
@@ -20,11 +36,10 @@ class FormatterProviderTest extends PHPUnit_Framework_TestCase
             'sample.rot',
             'sample.case'
         ], $formatter->formats());
+
         $this->assertTrue($formatter->hasFormat('number.unsigned'));
 
     }
-
-
 
     public function testInvalidFormatter()
     {
@@ -32,5 +47,12 @@ class FormatterProviderTest extends PHPUnit_Framework_TestCase
         $formatter = new Formatter([
             'fake' => new stdClass,
         ]);
+    }
+
+    public function testFormat()
+    {
+        $this->assertSame($this->formatter->format('foo', 'sample.return'), 'foo');
+
+        // $this->assertSame($this->formatter->format('foo', ['sample.case', SampleProvider::CASE_UPPER]), 'FOO');
     }
 }
